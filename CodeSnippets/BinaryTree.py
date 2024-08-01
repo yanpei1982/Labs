ad052c986421e78c70b9736ac692922a3d513c5d
yanpei1982 <yanpei.mail@gmail.com>
2024-08-01 13:54:00 +0800

    红黑二叉树

---
+++ CodeSnippets/BinaryTree.py
@@ -0,0 +1,144 @@
+class Node:
+    def __init__(self, data, color="red"):
+        self.data = data
+        self.color = color
+        self.left = None
+        self.right = None
+        self.parent = None
+
+class RedBlackTree:
+    def __init__(self):
+        self.TNULL = Node(0, "black")
+        self.root = self.TNULL
+
+    def insert(self, key):
+        node = Node(key)
+        node.parent = None
+        node.data = key
+        node.left = self.TNULL
+        node.right = self.TNULL
+        node.color = "red"
+
+        parent = None
+        current = self.root
+
+        while current != self.TNULL:
+            parent = current
+            if node.data < current.data:
+                current = current.left
+            else:
+                current = current.right
+
+        node.parent = parent
+        if parent is None:
+            self.root = node
+        elif node.data < parent.data:
+            parent.left = node
+        else:
+            parent.right = node
+
+        if node.parent is None:
+            node.color = "black"
+            return
+
+        if node.parent.parent is None:
+            return
+
+        self.fix_insert(node)
+
+    def fix_insert(self, k):
+        while k.parent.color == "red":
+            if k.parent == k.parent.parent.right:
+                u = k.parent.parent.left
+                if u.color == "red":
+                    u.color = "black"
+                    k.parent.color = "black"
+                    k.parent.parent.color = "red"
+                    k = k.parent.parent
+                else:
+                    if k == k.parent.left:
+                        k = k.parent
+                        self.right_rotate(k)
+                    k.parent.color = "black"
+                    k.parent.parent.color = "red"
+                    self.left_rotate(k.parent.parent)
+            else:
+                u = k.parent.parent.right
+
+                if u.color == "red":
+                    u.color = "black"
+                    k.parent.color = "black"
+                    k.parent.parent.color = "red"
+                    k = k.parent.parent
+                else:
+                    if k == k.parent.right:
+                        k = k.parent
+                        self.left_rotate(k)
+                    k.parent.color = "black"
+                    k.parent.parent.color = "red"
+                    self.right_rotate(k.parent.parent)
+            if k == self.root:
+                break
+        self.root.color = "black"
+
+    def left_rotate(self, x):
+        y = x.right
+        x.right = y.left
+        if y.left != self.TNULL:
+            y.left.parent = x
+
+        y.parent = x.parent
+        if x.parent is None:
+            self.root = y
+        elif x == x.parent.left:
+            x.parent.left = y
+        else:
+            x.parent.right = y
+        y.left = x
+        x.parent = y
+
+    def right_rotate(self, x):
+        y = x.left
+        x.left = y.right
+        if y.right != self.TNULL:
+            y.right.parent = x
+
+        y.parent = x.parent
+        if x.parent is None:
+            self.root = y
+        elif x == x.parent.right:
+            x.parent.right = y
+        else:
+            x.parent.left = y
+        y.right = x
+        x.parent = y
+
+    def __repr__(self):
+        if self.root is None:
+            return ''
+        content = '\n'  # to hold final string
+        cur_nodes = [self.root]  # all nodes at current level
+        cur_level = 1  # current level number
+        while True:
+            cur_content = ''  # to hold current level content
+            next_nodes = []  # all nodes at next level
+            for node in cur_nodes:
+                if node:
+                    cur_content += str(node.data) + ' '
+                    next_nodes.append(node.left)
+                    next_nodes.append(node.right)
+                else:
+                    cur_content += 'N '
+            if len(next_nodes) == 0:
+                break
+            cur_nodes = next_nodes
+            content += cur_content + '\n'
+        return content
+
+# 示例用法
+if __name__ == "__main__":
+    rbt = RedBlackTree()
+    elements = [20, 15, 25, 10, 5, 1, 30, 35, 40]
+    for element in elements:
+        rbt.insert(element)
+    print(rbt)
